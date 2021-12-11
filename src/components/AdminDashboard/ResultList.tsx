10645adca8319c40f5301b9bd02ab566e1a7ca4a
Divyanshu Garg <divyanshugarg36@gmail.com>
2021-12-11 17:46:53 +0530

    Update UI for Attendance and Result Module
    - QR code feature is working
    
    Signed-off-by: Divyanshu Garg <divyanshugarg36@gmail.com>

--- src/components/AdminDashboard/ResultList.tsx
+++ src/components/AdminDashboard/ResultList.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { IonButton, IonCol, IonGrid, IonRow, IonSelect, IonSelectOption, useIonToast } from "@ionic/react";
+import { IonBadge, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonChip, IonCol, IonGrid, IonIcon, IonInput, IonItem, IonLabel, IonRippleEffect, IonRow, IonSelect, IonSelectOption, IonText, IonToggle, useIonToast } from "@ionic/react";
 import { useStoreActions, useStoreState } from "easy-peasy";
-import { API, mapValue } from "../../constants";
+import { API, GENDER, mapValue, mergeSearch } from "../../constants";
 import Axios from "axios";
-import { Table } from "../Table";
+import { americanFootball, callSharp, megaphone, trophyOutline } from "ionicons/icons";
+import { GenderIcon } from "../../common";
 
 export const ResultList: React.FC<any> = ({ view = false }) => {
-  const [filterSport, setFilterSport] = useState('all');
+  const [filterSport, setFilterSport] = useState('none');
 
   const [showToast] = useIonToast();
   const users = useStoreState<any>(({ users }) => users);
   const sports = useStoreState<any>(({ sports }) => sports);
   const allEvents = useStoreState<any>(({ allEvents }) => allEvents);
   const storeAllEvents = useStoreActions<any>(({ storeAllEvents }) => storeAllEvents);
+  const updateModalProfileId = useStoreActions<any>((actions) => actions.updateModalProfileId);
+
+  const [search, setSearch] = useState('');
+
   const processedUsers: any = {};
   users.forEach((user: any) => { processedUsers[user._id] = user; });
   const processData = allEvents
@@ -63,64 +68,131 @@ export const ResultList: React.FC<any> = ({ view = false }) => {
 
   return (
     <IonGrid>
-      <Table
-        data={processData}
-        headings={["Jersy No.", "Sport", "Sport Type", "Name", "URN", "Phone No.", "Gender", "Course", "Branch", "Position"]}
-        searchKeys={[
-          "user.jerseyNo", "sportId.sportName", "sportId.sportType", "user.fullName",
-          "user.universityRoll", "user.phoneNumber", "user.gender", "user.course", "user.branch", "event.position"
-        ]}
-        filters={
-          <IonGrid>
-            <IonRow>
-              <IonCol>
-                <IonSelect
-                  interface="popover"
-                  value={filterSport}
-                  onIonChange={(e) => setFilterSport(e.detail.value)}
-                >
-                  <IonSelectOption value="all">All</IonSelectOption>
-                  {sports.map(({ _id, sportName }: any) => (<IonSelectOption key={_id} value={_id}>{sportName}</IonSelectOption>))}
-                </IonSelect>
+      <IonRow>
+        <IonCol sizeXl="8" sizeLg="6" sizeSm="12" sizeXs="12">
+          <IonItem>
+            <IonSelect
+              interface="popover"
+              value={filterSport}
+              onIonChange={(e) => setFilterSport(e.detail.value)}
+            >
+              <IonSelectOption value="none">Select Sport</IonSelectOption>
+              {sports.map(({ _id, sportName }: any) => (<IonSelectOption key={_id} value={_id}>{sportName}</IonSelectOption>))}
+            </IonSelect>
+          </IonItem>
+        </IonCol>
+        <IonCol sizeXl="4" sizeLg="6" sizeSm="12" sizeXs="12">
+          <IonItem>
+            <IonInput
+              onIonChange={(e: any) => setSearch(e.detail.value)}
+              placeholder="Search"
+              clearInput
+            />
+          </IonItem>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        {mergeSearch({
+          data: processData,
+          search,
+          options: {
+            keys: [
+              "user.jerseyNo", "sportId.sportName", "sportId.sportType", "user.fullName",
+              "user.universityRoll", "user.phoneNumber", "user.gender", "user.course", "user.branch", "event.position"
+            ]
+          }
+        })
+          .map((event: any) => {
+            const color = event.isSearched ? "light" : "";
+            const isMale = event.user.gender === GENDER[1].value;
+            return (
+              <IonCol key={event._id} sizeXl="3" sizeLg="4" sizeMd="6" sizeSm="12" size="12">
+                <IonCard className="ion-activatable ripple-parent" color={color} onClick={() => updateModalProfileId(event.user._id)}>
+                  <IonRippleEffect />
+                  <IonCardHeader>
+                    <IonItem color="transparent" lines="none">
+                      <IonCardSubtitle>Jersey {event.user.jerseyNo}</IonCardSubtitle>
+                      {view && (
+                        <IonChip style={{ padding: 0, margin: 0 }} color="transparent" slot="end">
+                          {[...Array.from({ length: event.position }, (_, i) => i + 1)].map((node) => (
+                            <IonIcon style={{ margin: 3, marginRight: 0 }} color="success" key={node} icon={trophyOutline}></IonIcon>
+                          ))}
+                        </IonChip>
+                      )}
+                    </IonItem>
+                    <IonItem color="transparent" lines="none">
+                      <IonCardTitle>{event.user.fullName}</IonCardTitle>
+                      <GenderIcon gender={event.user.gender} slot="end" />
+                    </IonItem>
+                  </IonCardHeader>
+                  <IonCardContent color={color}>
+                    <IonItem color="transparent" lines="none">
+                      <IonIcon color={isMale ? "tertiary" : "pink"} slot="start" icon={americanFootball} />
+                      <IonLabel>{event.sportId.sportName}</IonLabel>
+                    </IonItem>
+                    <IonItem color="transparent" lines="none">
+                      <IonIcon color={isMale ? "tertiary" : "pink"} slot="start" icon={megaphone} />
+                      <IonLabel>{mapValue("SPORT_TYPE", event.sportId.sportType)}</IonLabel>
+                    </IonItem>
+                    <IonItem color="transparent" lines="none">
+                      <IonBadge color={isMale ? "tertiary" : "pink"} slot="start">URN</IonBadge>
+                      <IonLabel>{event.user.universityRoll}</IonLabel>
+                    </IonItem>
+                    <IonItem color="transparent" lines="none">
+                      <IonIcon color={isMale ? "tertiary" : "pink"} slot="start" icon={callSharp} />
+                      <IonLabel>{event.user.phoneNumber}</IonLabel>
+                    </IonItem>
+                    {!view && (
+                      <IonSelect interface="popover"
+                        onClick={(e) => e.stopPropagation()}
+                        onIonChange={e => selectResult({ _id: event._id, value: e.detail.value })}
+                        value={(() => {
+                          const found = result.find((node: any) => node._id === event._id);
+                          if (!found) {
+                            return 0;
+                          }
+                          return found.value;
+                        })()}
+                      >
+                        <IonSelectOption value={0}>None</IonSelectOption>
+                        <IonSelectOption value={1}>First</IonSelectOption>
+                        <IonSelectOption value={2}>Second</IonSelectOption>
+                        <IonSelectOption value={3}>Third</IonSelectOption>
+                      </IonSelect>
+                    )}
+                  </IonCardContent>
+                </IonCard>
               </IonCol>
-            </IonRow>
-          </IonGrid>
-        }
-      >
-        {(data: any) => data.map((event: any) => (
-          <tr key={event._id}>
-            <td>{event.user.jerseyNo}</td>
-            <td>{event.sportId.sportName}</td>
-            <td>{mapValue("SPORT_TYPE", event.sportId.sportType)}</td>
-            <td>{event.user.fullName}</td>
-            <td>{event.user.universityRoll}</td>
-            <td>{event.user.phoneNumber}</td>
-            <td>{mapValue("GENDER", event.user.gender)}</td>
-            <td>{mapValue("COURSE", event.user.course)}</td>
-            <td>{mapValue("BRANCH", event.user.branch)}</td>
-            <td>
-              {view ? mapValue("RESULT", event.position) : (
-                <IonSelect interface="popover"
-                  onIonChange={e => selectResult({ _id: event._id, value: e.detail.value })}
-                  value={(() => {
-                    const found = result.find((node: any) => node._id === event._id);
-                    if (!found) {
-                      return 0;
-                    }
-                    return found.value;
-                  })()}
+            )
+          })}
+      </IonRow>
+      <IonGrid>
+        {filterSport === "none" && <IonText color="danger">Please select an sport to view the list</IonText>}
+        {filterSport !== "none" && processData.length === 0 && <IonText color="danger">No records found</IonText>}
+      </IonGrid>
+      {processData.length !== 0 && (
+        <IonGrid>
+          <IonLabel>Final Result</IonLabel>
+          <IonRow>
+            {processData.map((event: any) => {
+              const position = result.find((node: any) => event._id === node._id)?.value;
+              return position ? (
+                <IonBadge
+                  key={event._id}
+                  style={{ margin: 3 }}
+                  color="success"
                 >
-                  <IonSelectOption value={0}>None</IonSelectOption>
-                  <IonSelectOption value={1}>First</IonSelectOption>
-                  <IonSelectOption value={2}>Second</IonSelectOption>
-                  <IonSelectOption value={3}>Third</IonSelectOption>
-                </IonSelect>
-              )}
-            </td>
-          </tr>
-        ))}
-      </Table>
-      {!view && <IonButton expand="block" onClick={markResult} >Save Result</IonButton>}
-    </IonGrid>
+                  {`Jersey ${event.user.jerseyNo} : `}
+                  {[...Array.from({ length: position }, (_, i) => i + 1)].map((node) => (
+                    <IonIcon key={node} icon={trophyOutline}></IonIcon>
+                  ))}
+                </IonBadge>
+              ) : " "
+            })}
+          </IonRow>
+        </IonGrid>
+      )}
+      {!view && processData.length !== 0 && <IonButton expand="block" onClick={markResult}>Save Result</IonButton>}
+    </IonGrid >
   );
 };
