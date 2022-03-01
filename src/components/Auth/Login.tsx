3813aabd46af0079d788db0054116c7dc7078c7c
Amanjot Singh <theamanjs@gmail.com>
2022-03-02 02:21:03 +0530

    fix: remove default usename password

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -13,8 +13,8 @@ import {
 
 export const Login: React.FC<any> = ({ onSubmit }) => {
   const router = useIonRouter();
-  const [email, setEmail] = useState('amanjot1815119@gndec.ac.in');
-  const [password, setPassword] = useState('this.Unlock(7)');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div className="card-container">
