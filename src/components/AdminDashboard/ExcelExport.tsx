2c494f8d6c82414fca158745dfe4df8cbbaef0f5
Divyanshu Garg <divyanshugarg36@gmail.com>
2022-03-07 21:18:04 +0530

    chore: fix event excel layout

--- src/components/AdminDashboard/ExcelExport.tsx
+++ src/components/AdminDashboard/ExcelExport.tsx
@@ -127,7 +127,7 @@ export const ExcelExport: React.FC<any> = () => {
   const departmentFileName = `${currentCourse} - ${currentBranch}`
 
   const eventFileLayout = [
-    // { label: "Jersey No.", value: "jerseyNo" },
+    { label: "Jersey No.", value: "jerseyNo" },
     { label: "Name", value: "fullName" },
     { label: "Branch", value: "branch" },
     { label: "URN", value: "universityRoll" },
